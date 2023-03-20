0fb6306aeda34635b796c5a4bc8d1b0ab23343ce
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-20 15:10:58 +0100

    Code refactorisation

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -6,14 +6,14 @@ app.get('/', (req, res) => {
     var sentText = "";
     
     
-    if(req.query.videoPlayer) {
+    if (req.query.videoPlayer) {
       sentText += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
  
       sentText += `<button id="videoCancel" onclick="changeOnSrc("videoPlayer", "cancel.mp4")"> Cancel video </button>`
 
     }
     
-    if(req.query.audioPlayer) {
+    if (req.query.audioPlayer) {
       sentText += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
  
       sentText += `<button id="audioCancel" onclick="changeOnSrc("audioPlayer", "cancel.mp3")"> Cancel audio </button>`
@@ -21,8 +21,8 @@ app.get('/', (req, res) => {
       
     }
     
-    if(req.query.imgFile) {
-      sentText += `<img id="posterImage" src="` + res.query.imgFile+  `"></img>`
+    if (req.query.imgFile) {
+      sentText += `<img id="posterImage" src="` + res.query.imgFile +  `"></img>`
     }
 
     res.send(sentText);
