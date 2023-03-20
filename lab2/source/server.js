e59b197f6a733764bc9a8098fd31e6b842ed5a03
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-20 15:00:12 +0100

    Implementacja lab1

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -3,7 +3,18 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    var sentText = "";
+    
+    
+    if(req.query.videoPlayer) {
+      sentText += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
+    }
+    
+    if(req.query.audioPlayer) {
+      sentText += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
+    }
+
+    res.send(sentText);
 })
 
 app.listen(4080)
