9372541d41782b4f195f022f683b90708d3052ec
SzymonB5 <121040955+SzymonB5@users.noreply.github.com>
2023-03-20 15:13:20 +0100

    Ending task

--- lab2/source/server.js
+++ lab2/source/server.js
@@ -3,29 +3,31 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    var sentText = "";
+    var toSend = "";
     
     
     if (req.query.videoPlayer) {
-      sentText += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
+      toSend += `<video id=videoPlayer src=` + res.query.videoPlayer + `> </video>`
  
-      sentText += `<button id="videoCancel" onclick="changeOnSrc("videoPlayer", "cancel.mp4")"> Cancel video </button>`
+      toSend += `<button id="videoCancel" onclick="changeOnSrc("videoPlayer", "cancel.mp4")"> Cancel video </button>`
 
     }
     
     if (req.query.audioPlayer) {
-      sentText += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
+      toSend += `<audio id=audioPlayer src=` + res.query.audioPlayer + `> </audio>`
  
-      sentText += `<button id="audioCancel" onclick="changeOnSrc("audioPlayer", "cancel.mp3")"> Cancel audio </button>`
+      toSend += `<button id="audioCancel" onclick="changeOnSrc("audioPlayer", "cancel.mp3")"> Cancel audio </button>`
 
       
     }
     
-    if (req.query.imgFile) {
-      sentText += `<img id="posterImage" src="` + res.query.imgFile +  `"></img>`
-    }
-
-    res.send(sentText);
+    var tag = `<script>
+      function changeOnSrc(id, onClickValue) {
+        element = Document.getElementById(id);
+        element.src = onClickValue; 
+      }
+    </script>`
+    res.send(toSend + tag);
 })
 
 app.listen(4080)
