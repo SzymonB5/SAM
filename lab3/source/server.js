70be66847fc6cc9525642e6e05b0475c12610979
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 14:27:55 +0200

    Powrót do lab2

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -3,7 +3,35 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+
+	let sentMessage = '';
+	
+	if (req.query.videoFile) {
+		sentMessage += `</br> <video id = "videoPlayer" src = ${req.query.videoFile}></video>`;
+		sentMessage += `<button type="button" id = "videoCancel">Click Me!</button>`;
+		sentMessage += `<script>document.getElementById("videoCancel").addEventListener('click', () => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
+	}
+	
+	if (req.query.audioFile) {
+		sentMessage += `<audio id = "audioPlayer" src = ${req.query.audioFile}></audio>`;
+		sentMessage += `<button type="button" id = "audioCancel">Click Me!</button>`;
+		sentMessage += `<script>document.getElementById("audioCancel").addEventListener('click', () => document.getElementById("audioPlayer").src = "cancel.mp4")</script>`
+	}
+	
+	if (req.query.imgFile) {
+		sentMessage += `<img src=${req.query.imgFile} id = "posterImage">`;
+	}
+
+
+
+    res.send(sentMessage);
 })
 
+
+app.get('/', (req, res) => {
+	res.send("Hello World!");
+})
+
+
 app.listen(4080)
+
