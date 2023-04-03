f017acc70cf8f6456af18dba34ad3d29a8c5ac91
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 15:24:17 +0200

    Final commit

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -15,16 +15,14 @@ app.get('/', (req, res) => {
 		messageToSend += `</br> <video id = "videoPlayer" src = ${req.query.videoFile}></video>`;
 		messageToSend += `<button type="button" id = "videoCancel">cancel video</button>`;
 		messageToSend += `<button type="button" id = "videoAdd">Add video</button>`;
-		messageToSend += `<script>document.getElementById("videoCancel").addEventListener('click', 
-		() => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
+		messageToSend += `<script>document.getElementById("videoCancel").addEventListener('click', () => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
 	}
 	
 	if (req.query.audioFile) {
 		messageToSend  += `<audio id = "audioPlayer" src = ${req.query.audioFile}></audio>`;
 		messageToSend  += `<button type="button" id = "audioCancel">cancel audio</button>`;
 		messageToSend  += `<button type="button" id = "audioAdd">Add audio</button>`;
-		messageToSend  += `<script>document.getElementById("audioCancel").addEventListener('click', 
-		() => document.getElementById("audioPlayer").src = "cancel.mp3")</script>`
+		messageToSend  += `<script>document.getElementById("audioCancel").addEventListener('click', () => document.getElementById("audioPlayer").src = "cancel.mp3")</script>`
 
 	}
 	
