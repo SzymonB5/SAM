5c4d140ce8c3b792edcd07dff85f3c303286f89a
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 15:14:49 +0200

    Fixing errors

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -1,8 +1,6 @@
 const express = require('express')
 
-
 const app = express()
-
 
 app.get('/', (req, res) => {
 
@@ -65,9 +63,6 @@ app.get('/', (req, res) => {
 
     	res.send(messageToSend);
 })
-
-
-
 
 app.get('/', (req, res) => {
 	res.send("Hello World!");
