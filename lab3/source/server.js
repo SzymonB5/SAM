f9631ae75d2dc6f794e9f282423850ac6168669e
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-03 14:58:43 +0200

    Commit

--- lab3/source/server.js
+++ lab3/source/server.js
@@ -6,20 +6,53 @@ app.get('/', (req, res) => {
 
 	let messageToSend = '';
 	
+	
 	if (req.query.videoFile) {
 		messageToSend += `</br> <video id = "videoPlayer" src = ${req.query.videoFile}></video>`;
-		messageToSend += `<button type="button" id = "videoCancel">Click Me!</button>`;
-		messageToSend += `<script>document.getElementById("videoCancel").addEventListener('click', () => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
+		messageToSend += `<button type="button" id = "videoCancel">cancel video</button>`;
+		messageToSend += `<button type="button" id = "videoAdd">Add video</button>`;
+		messageToSend += `<script>document.getElementById("videoCancel").addEventListener('click', 
+												() => document.getElementById("videoPlayer").src = "cancel.mp4")</script>`
+		
+		messageToSend += `<script defer>document.getElementById("videoAdd").addEventListener('click', 
+				() => {
+	    			let table = document.getElementById("playlist_table");
+    				let row = table.insertRow(localStorage.i);
+    				let c1 = row.insertCell(0);
+				let c2 = row.insertCell(1);
+		           	let c3 = row.insertCell(2);
+        		    c1.innerHTML = "${localStorage.i + 1}"
+		            c2.innerHTML = "${document.getElementById("audioPlayer").src}"
+        		    c3.innerText = "Audio"
+		            localStorage.i += 1;
+		}</script>`
 	}
+	
+	
 	
 	if (req.query.audioFile) {
 		messageToSend += `<audio id = "audioPlayer" src = ${req.query.audioFile}></audio>`;
-		messageToSend += `<button type="button" id = "audioCancel">Click Me!</button>`;
-		messageToSend += `<script>document.getElementById("audioCancel").addEventListener('click', () => document.getElementById("audioPlayer").src = "cancel.mp4")</script>`
+		messageToSend += `<button type="button" id = "audioCancel">cancel audio</button>`;
+		messageToSend += `<button type="button" id = "audioAdd">Add audio</button>`;
+		messageToSend += `<script>document.getElementById("audioCancel").addEventListener('click', 
+			() => document.getElementById("audioPlayer").src = "cancel.mp4")</script>`
+		messageToSend += `<script defer>document.getElementById("audioAdd").addEventListener('click', 
+		() => {
+    		let table = document.getElementById("playlist_table");
+    		let row = table.insertRow(localStorage.i);
+    		let c1 = row.insertCell(0);
+		let c2 = row.insertCell(1);
+            	let c3 = row.insertCell(2);
+	            c1.innerHTML = "${localStorage.i + 1}"
+        	    c2.innerHTML = "${document.getElementById("audioPlayer").src}"
+	            c3.innerText = "Audio"
+        	    localStorage.i += 1;
+		}</script>`
 	}
 	
 	if (req.query.imgFile) {
 		messageToSend += `<img src=${req.query.imgFile} id = "posterImage">`;
+		messageToSend += `<button type="button" id = "imgAdd">Add img</button>`;
 	}
 	
 	messageToSend += `<table id = 'playlist_table'> 
@@ -29,7 +62,7 @@ app.get('/', (req, res) => {
 	
 
 
-    res.send(messageToSend);
+    	res.send(messageToSend);
 })
 
 
