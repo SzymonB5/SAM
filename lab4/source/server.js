4f93710100af08f448d94006cc0ebad7d3fc9ff2
SzymonB5 <235831@edu.p.lodz.pl>
2023-04-24 14:33:05 +0200

    Small correction

--- lab4/source/server.js
+++ lab4/source/server.js
@@ -3,14 +3,6 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-
-app.listen(4080)const express = require('express')
-
-const app = express()
-
-app.get('/', (req, res) => {
 
 	let messageToSend  = '';
 	
